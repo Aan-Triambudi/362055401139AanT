046a74b9e68d428531ad97894bac1335938d3677
FlutLab.io <>
2022-02-25 02:59:12 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  runApp(MyApp());
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-
-  const MyHomePage({@required this.title});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: Stack(
+        children: <Widget>[
+          Image.asset(
+            'aset/images/space.jpeg',
+            height: 1000,
+            fit: BoxFit.fitHeight,
+          ),
+          Positioned(
+            top: 180,
+            left: 135,
+            child: CircleAvatar(
+              radius: 75,
+              backgroundImage: AssetImage('aset/images/small.jpeg'),
+            ),
+          ),
+        ],
       ),
     );
   }
